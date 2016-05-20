a89775784e3740844d90b2754f00b946a37d772b
Jason Song <wolfogre@outlook.com>
2016-05-20 11:56:36 +0800

    Add countly support

--- BingWallpaper/MainForm.cs
+++ BingWallpaper/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CountlySDK;
+using System;
 using System.Windows.Forms;
 
 namespace BingWallpaper
@@ -14,22 +15,27 @@ namespace BingWallpaper
 
 		private void MainForm_Load(object sender, EventArgs e)
 		{
+			Countly.StartSession("http://countly.wolfogre.com", "bdb2627a3e46353c93c60c566f045cc4d0c99005");
 			paperGetter = new PaperGetter(notifyIcon);
+			Countly.RecordEvent("Start");
 			paperGetter.Start();
 		}
 
 		private void tsmiRetry_Click(object sender, EventArgs e)
 		{
+			Countly.RecordEvent("Retry");
 			paperGetter.Start();
 		}
 
 		private void tsmiAbout_Click(object sender, EventArgs e)
 		{
+			Countly.RecordEvent("Abort");
 			System.Diagnostics.Process.Start("http://blog.wolfogre.com/2016/04/11/280.html");
 		}
 
 		private void tsmiQuit_Click(object sender, EventArgs e)
 		{
+			Countly.RecordEvent("Quit");
 			paperGetter.Abort();
 			Application.Exit();
 		}
